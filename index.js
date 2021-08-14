db50ee9b1802a437480b75de6873874cecfc9e46
Gerardo <monologan@gmail.com>
2021-08-14 14:20:47 -0500

    manipulacion con las cards

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//cambia lo que tiene escrito el boton.
+//home
+
 const boton = document.getElementById('buy-now')
 
 boton.addEventListener('click', () => {
@@ -5,24 +8,59 @@ boton.addEventListener('click', () => {
         boton.innerHTML = 'Gracias por tu compra'
     } else {
         boton.innerHTML = 'comprar ahora'
-
     }
-
-
 })
+//end
 
-const cards = document.querySelectorAll('.card')
-console.log(cards);
+//funcion que agrega y elimina cards
+//home
+const renderCatCard = (cardIndex) => {
+    const card = document.createElement('div')
+    card.classList.add('card')
 
-for (let i = 0; i < cards.length; i++) {
-    const card = cards[i];
+    const image = document.createElement('img')
+    image.src = 'https://cataas.com/cat'
+    image.alt = 'Catsuky'
 
+    card.appendChild(image)
+    cardCatalogue.appendChild(card)
+
+    //agrego el indicador de indice en la card
     const indexElement = document.createElement('p');
     card.appendChild(indexElement);
-    indexElement.innerHTML = i + 1
-
+    indexElement.innerHTML = cardIndex + 1
+    //agrego la funcionalidad de eliminar la card al hacer click
     card.addEventListener('click', () => {
         card.remove()
     })
-    
+
 }
+
+//end
+
+const cardCatalogue = document.getElementsByClassName('catalogue-content')[0]
+const addButton = document.getElementById('add-cat-button')
+addButton.addEventListener('click',renderCatCard)
+//empieza a cargar
+//muestra un indicador de que esta cargando
+//una vez que carga muestra los elementos
+const getCatCards = () => {
+    //traer las cards de gatos de la base de datos
+
+    setTimeout(() => {
+
+        const loader = document.getElementById('catalogue-loader')
+
+        loader.style.display = 'none'
+
+        const cardCats = [1, 2, 3]
+        for (let i = 0; i < cardCats.length; i++) {
+            renderCatCard(i)
+
+        }
+    }, 5000)
+
+
+}
+//cargar las cards de gatos y mostrarlas en pantalla.
+getCatCards()
